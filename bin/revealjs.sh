4f090c182333c2a0560c018a1069545ea5fac297
xieby1 <xieby1@users.noreply.github.com>
2025-09-24 15:48:26 +0000

    deploy: 56366acf86980ad5f7d985da4d1df1df0f589b84

---
+++ bin/revealjs.sh
@@ -0,0 +1,164 @@
+#!/usr/bin/env bash
+
+set -e # exit on error
+
+# kill children processes when exit
+# https://aweirdimagination.net/2020/06/28/kill-child-jobs-on-script-exit/
+cleanup() {
+    pkill -P $$ || true # kill all processes whose parent is this process
+}
+for sig in INT QUIT HUP TERM; do
+  trap "
+    cleanup
+    trap - $sig EXIT
+    kill -s $sig "'"$$"' "$sig"
+done
+trap cleanup EXIT
+
+TEMPLATE="$(dirname $(realpath --relative-to=. $0))/../share/markdown_revealjs/template.html"
+INCLUDE_FILES="$(dirname $(realpath --relative-to=. $0))/../lib/lua-filters/include-files/include-files.lua"
+INCLUDE_CODE_FILES="$(dirname $(realpath --relative-to=. $0))/../lib/lua-filters/include-code-files/include-code-files.lua"
+REVEALJS_CODEBLOCK="$(dirname $(realpath --relative-to=. $0))/../lib/lua-filters/revealjs-codeblock/revealjs-codeblock.lua"
+INLINE_IMAGE="$(dirname $(realpath --relative-to=. $0))/../share/markdown_revealjs/inline-image.lua"
+
+DAEMONIZE=0
+PORT=7782
+ARGS=()
+while [[ $# -gt 0 ]]; do
+  case "$1" in
+    -d)
+      DAEMONIZE=1
+      shift  # 跳过-d参数
+      ;;
+    -p)
+      shift
+      PORT="$1"
+      shift
+      ;;
+    *)
+      ARGS+=("$1")  # 将其他参数加入数组
+      shift
+      ;;
+  esac
+done
+
+MD=${ARGS[0]}
+
+usage() {
+    echo "Usage: ${0##*/}" convert markdown file to reveal.js slides.
+    echo "[REPOROOT=<Path>] ${0##*/} [-h] [-d] <input.md> [pandoc args]"
+    echo "  Use pandoc convert <input.md> to input.html."
+    echo "  -d          daemonize"
+    echo "  -p <port>   daemonize port default 7782"
+    echo "  Environment variables:"
+    echo "  REPOROOT    override the default markdown_revealjs url"
+    echo "              E.g. if you want to play your slides offline,"
+    echo "              set the REPOROOT to the local markdown_revealjs."
+    echo "  PANDOC      override the default pandoc executable"
+    echo "  Customized pandoc args:"
+    echo "  -V lxgw     enable LXGW Wenkai font"
+    exit 0
+}
+
+revealjs() {
+if [[ -n ${REPOROOT} ]]; then
+    echo REPOROOT is set as \"${REPOROOT}\"
+else
+    REPOROOT="https://xieby1.github.io/markdown_revealjs"
+fi
+
+if [[ -n ${PANDOC} ]]; then
+    echo PANDOC is set as \"${PANDOC}\"
+else
+    PANDOC=pandoc
+fi
+
+if [[ $# -eq 0 || "$1" == "-h" || -z ${MD} ]]; then
+  usage
+fi
+
+# get options from front matter in MD
+tripleDash=0
+while read -r line; do
+    if [[ ${line} =~ ^--- ]]; then
+        tripleDash+=1
+        continue
+    fi
+
+    if [[ ${tripleDash} -gt 1 ]]; then
+        break
+    elif [[ ${tripleDash} -eq 1 ]]; then
+        opt=${line%%:*}
+        val=${line##*:}
+        # remove leading spaces
+        val=$(echo ${val})
+
+        case ${opt} in
+        toc-depth)
+            TOC_DEPTH=${val}
+        ;;
+        pandoc-opts)
+            PANDOC_OPTS=${val}
+        ;;
+        esac
+    fi
+done < ${MD}
+
+CMD=(
+    # replace $reporoot-url$ with ${REPOROOT}
+    "sed 's,\\\$reporoot-url\\\$,${REPOROOT},g' ${MD}"
+
+    "|" # pipeline
+
+    "${PANDOC}"
+    "-t revealjs"
+    "--template=${TEMPLATE}"
+    "-V theme=white"
+    "-V code-theme=stackoverflow-light"
+    "-V reporoot-url=${REPOROOT}"
+    "-V width=1200"
+    "-V height=700"
+    "-V hash"
+    "-V chalkboard"
+    "-V touch=false"
+    "-V simplemenu"
+    "-V menu"
+    "-V verticator"
+    "-V showSlideNumber=all"
+    "-V daemonize-port=${PORT}"
+    "--slide-level=6"
+    # table of content
+    "--toc"
+    "--toc-depth=${TOC_DEPTH:=2}"
+    # number sections
+    "-N"
+    # include files
+    "-L" "${INCLUDE_FILES}"
+    # include cide files
+    "-L" "${INCLUDE_CODE_FILES}"
+    # use revealjs provided highlight (highlight.js)
+    "-L" "${REVEALJS_CODEBLOCK}"
+    "-L" "${INLINE_IMAGE}"
+    "--no-highlight"
+    "--mathjax"
+    "-o ${MD%.*}.html"
+    "${PANDOC_OPTS}"
+    "${@:2}"
+)
+if [[ $DAEMONIZE == 1 ]]; then
+  CMD+=("-V daemonize")
+fi
+
+eval "${CMD[@]}"
+# echo "${CMD[@]}"
+}
+
+if [[ $DAEMONIZE == 1 ]]; then
+  browser-sync start --port "$PORT" -s . -f "$MD" --index "${MD%.*}.html" &
+  while true; do
+    revealjs "${ARGS[@]}"
+    inotifywait -e modify "$MD" || true
+  done
+else
+    revealjs "${ARGS[@]}"
+fi
---
+++ share/markdown_revealjs/inline-image.lua
@@ -0,0 +1,107 @@
+-- TODO: enhance tonumber to handle '%' for scale and clip
+local function to_px(value_unit)
+  local value = tonumber(string.match(value_unit, "%d+"))
+  local unit = string.match(value_unit, "%a+")
+  -- print(string.format("value_unit %s, value %d, unit %s", value_unit, value, unit))
+  if unit == "cm" then
+    value = value * 25.2 / 64
+  elseif unit == "mm" then
+    value = value * 25.2 / 64 / 10
+  elseif unit == "Q" then
+    value = value * 25.2 / 64 / 40
+  elseif unit == "in" then
+    value = value
+  elseif unit == "pc" then
+    value = value / 6
+  elseif unit == "pt" then
+    value = value / 72
+  elseif unit == "px" then
+    value = value / 96
+  end
+  return value * 96
+end
+function Inlines(inlines)
+  if #inlines==1 and inlines[1].tag=="Image" then
+    local img = inlines[1]
+    local ext = string.sub(img.src, -3, -1)
+
+    local img_width
+    local img_height
+    local div_content
+    if ext == "svg" then
+      -- svg = "<svg ...>...</svg>"
+      div_content = (function() -- begin of a local scope
+        -- handle ~ in img.src
+        local path = img.src
+        if string.sub(path, 1, 1) == "~" then
+          path = os.getenv("HOME") .. string.sub(path, 2)
+        end
+
+        local file = io.open(path, "r")
+        if not file then return "inline-image.lua: NOT FOUND: " .. img.src end
+        local content = file:read("*a")
+        file:close()
+        -- Pattern to match the entire <svg> tag and its content
+        return content:match("<svg.*</svg>")
+      end) () -- end of a local scope
+      img_width = string.match(div_content, '<svg[^>]*width=["\']?([^ >"\']*)')
+      img_height = string.match(div_content, '<svg[^>]*height=["\']?([^ >"\']*)')
+    else
+      return nil -- This means that the pandoc object should remain unchanged.
+    end
+
+    local scale=1
+    if img.attr.attributes.scale then
+      scale = tonumber(img.attr.attributes.scale)
+    elseif img.attr.attributes.width then
+      scale = to_px(img.attr.attributes.width) / to_px(img_width)
+    elseif img.attr.attributes.height then
+      scale = to_px(img.attr.attributes.height) / to_px(img_height)
+    end
+
+    local html_content = string.format(
+      [[<div style="transform:scale(%f);
+        margin:%fpx %fpx;
+        display:flex; justify-content:center;"
+      >%s</div>]],
+      scale, (scale-1)/2*to_px(img_height), (scale-1)/2*to_px(img_width), div_content
+    )
+
+    local width = scale * to_px(img_width)
+    local height = scale * to_px(img_height)
+    if img.attr.attributes.clip then
+      local t,r,b,l = string.match(img.attr.attributes.clip, '(%d+)%%%s*(%d+)%%%s*(%d+)%%%s*(%d+)%%')
+      t=tonumber(t); r=tonumber(r); b=tonumber(b); l=tonumber(l);
+      -- print("clip: ", t, r, b, l)
+      html_content = string.format(
+        [[<div style="display:flex; justify-content:center;">
+          <div style="overflow:hidden; width:%fpx; height:%fpx;
+            mask-mode:alpha; mask-composite:intersect;%s
+          ">
+          <div style="transform:translate(%fpx,%fpx); width:%fpx; height:%fpx;">
+          %s
+          </div>
+          </div>
+          </div>]],
+        width*(r-l)/100, height*(b-t)/100,
+        (function ()
+          local gradients = {}
+          if t~=0 then   table.insert(gradients, "linear-gradient(to bottom,transparent 0%, black 10%)") end
+          if r~=100 then table.insert(gradients, "linear-gradient(to left,  transparent 0%, black 10%)") end
+          if b~=100 then table.insert(gradients, "linear-gradient(to top,   transparent 0%, black 10%)") end
+          if l~=0 then   table.insert(gradients, "linear-gradient(to right, transparent 0%, black 10%)") end
+          local gradients_str = table.concat(gradients, ",")
+          if gradients_str ~= "" then
+            gradients_str = string.format("mask-image:%s;", gradients_str)
+          end
+          -- print(gradients_str)
+          return gradients_str
+        end)(),
+        width*( -l)/100, height*( -t)/100, width, height,
+        html_content
+      )
+    end
+    inlines[1] = pandoc.RawInline("html", html_content)
+  end
+  return inlines
+end
