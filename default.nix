51319f0e52e68ad6800450d9ce4958a4401a59de
xieby1 <xieby1@outlook.com>
2026-01-06 09:32:24 +0800

    update default.nix

--- default.nix
+++ default.nix
@@ -5,13 +5,11 @@ in
 builtins.derivation {
   name = "revealjs.sh";
   system = builtins.currentSystem;
-  bin = ./bin;
-  share = ./share;
+  src = ./.;
   builder = pkgs.writeShellScript "revealjs_sh_builder" ''
     source ${pkgs.stdenv}/setup
     mkdir -p $out
-    cp -r $bin $out/bin
-    cp -r $share $out/share
+    cp -r $src/{bin,share,lib,fonts} $out/
 
     chmod -R +w $out/bin
     echo sed -i 's,=pandoc,=${pkgs.pandoc}/bin/pandoc,g' $out/bin/revealjs.sh
